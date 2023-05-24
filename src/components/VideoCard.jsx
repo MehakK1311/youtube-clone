3b99ef515ada40f18efad64eca51535c5cd6f019
MehakK1311 <mehakhanna1311@gmail.com>
2023-05-24 20:34:06 +0530

    feat: Feed

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,48 @@
+import { abbreviateNumber } from "js-abbreviation-number";
+import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+// import VideoLength from "../shared/videoLength";
+
+
+const VideoCard = ({video}) => {
+  return (
+    <Link to={`/video/${video?.postId}`}>
+            <div className="flex flex-col mb-8">
+                <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+                    <img
+                        className="h-full w-full object-cover"
+                        src={video?.submission?.thumbnail}
+                    />
+                </div>
+                <div className="flex text-white mt-3">
+                    <div className="flex items-start">
+                        <div className="flex h-9 w-9 rounded-full overflow-hidden">
+                            <img
+                                className="h-full w-full object-cover"
+                                src={video?.creator?.pic}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col ml-3 overflow-hidden">
+                        <span className="text-sm font-bold line-clamp-2">
+                            {video?.submission?.title}
+                        </span>
+                        <span className="text-[12px] font-semibold mt-2 text-white/[0.7] flex items-center">
+                            {video?.creator?.name}
+                        </span>
+                        <div className="flex text-[12px] font-semibold text-white/[0.7] truncate overflow-hidden">
+                            <span>{`${abbreviateNumber(
+                                video?.reaction?.count,
+                                2
+                            )} reactions`}</span>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+export default VideoCard
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 
 import { Context } from "../context/contextApi";
 import LeftNav from "../components/LeftNav";
-import VideoCard from "../components/VedioCard";
+import VideoCard from "../components/VideoCard";
 
 const Feed = () => {
   const { loading, searchResults } = useContext(Context);
@@ -18,12 +18,9 @@ const Feed = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
           {!loading &&
             searchResults.map((item) => {
-              if (item.type !== "video") return false;
               return (
-                <VideoCard
-                  key={item?.video?.videoId}
-                  video={item?.video}
-                />
+                // console.log(item)
+                <VideoCard key={item.postId} video={item}/>
               );
             })}
         </div>
